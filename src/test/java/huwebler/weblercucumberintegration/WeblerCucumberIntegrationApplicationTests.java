704904c3b0fb0c8f7327e126e60684370b1db638
Szőke Roland <szokeroland0@gmail.com>
2024-04-06 17:16:54 +0200

    Initial commit

---
+++ src/test/java/huwebler/weblercucumberintegration/WeblerCucumberIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package huwebler.weblercucumberintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerCucumberIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
